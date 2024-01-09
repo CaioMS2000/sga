026e538be1b217d4e15571da458039ca3a319092
Caio M. S <caiocpe@gmail.com>
2024-01-09 17:42:19 -0300

    frontend: populando a página inicial do admin

--- frontend/src/app/dashboard/admin/page.tsx
+++ frontend/src/app/dashboard/admin/page.tsx
@@ -1,4 +1,6 @@
 import { PropsWithChildren } from 'react';
+import { BsPersonFillGear } from 'react-icons/bs';
+import { MenuItemType } from '@/components/SideBar';
 
 interface AdminProps extends PropsWithChildren{
 }
@@ -7,7 +9,24 @@ export default async function Admin({}:AdminProps){
 
   return(
       <>
-      <h1>Admin page</h1>
+      <div className="grid grid-cols-3 gap-4">
+				{menuItems.map((item, index) => (
+					<a key={index} href={item.url}>
+						<div className="flex items-center w-fit gap-6 justify-around p-4 border-2 rounded-lg bg-byzantineBlue">
+							<item.icon className='text-white w-[16px] h-[16px] min-w-[16px] min-h-[16px]'/>
+							<p className="text-white font-bold">{item.label}</p>
+						</div>
+					</a>
+				))}
+			</div>
       </>
   )
 }
+
+const menuItems: MenuItemType[] = [
+	{
+		icon: BsPersonFillGear,
+		label: "Gerenciar usuários",
+		url: "/dashboard/admin/users",
+	},
+];
