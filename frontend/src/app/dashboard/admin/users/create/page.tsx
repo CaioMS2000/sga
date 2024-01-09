b065eae9061bcf6bfea4a41934ba558401a60dc0
Caio M. S <caiocpe@gmail.com>
2024-01-09 17:04:14 -0300

    frontend: reajuste na criação de usuarios

--- frontend/src/app/dashboard/admin/users/create/page.tsx
+++ frontend/src/app/dashboard/admin/users/create/page.tsx
@@ -2,7 +2,6 @@
 import { GET_DEPARTMENTS } from "@/lib/query/department";
 import { DepartmentModel } from "@/models/departmentModel";
 import { departmentFromCode, fetchGraphQL, stringToRole } from "@/utils";
-import CreateUserButton from "@/components/CreateUserButton";
 import {
 	Admin,
 	Analyst,
@@ -17,12 +16,15 @@ import { ChangeEvent, PropsWithChildren, useEffect, useState } from "react";
 import { DepartmentSelector } from "@/components/DepartmentSelector";
 import { RolesSelector } from "@/components/RolesSelector";
 import { CustomSelector } from "@/components/Selector";
+import { CREATE_USER } from "@/lib/mutation/user";
+import Toast from "@/components/Toast";
 
 type RoleOrNone = "none" | Role;
 
 interface CreateUserProps extends PropsWithChildren {}
 
 export default function CreateUser({}: CreateUserProps) {
+	const [createUserFlag, setCreateUserFlag] = useState(false)
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
@@ -62,28 +64,58 @@ export default function CreateUser({}: CreateUserProps) {
 		setDepartments(res);
 	}
 
+	function clearInputs(){
+		setName('')
+		setEmail('')
+		setPassword('')
+		setSelectedRole(['none'])
+		setSelectedDepartment('')
+	}
+	
+	async function handleCreateUser() {
+
+		const user = await fetchGraphQL(CREATE_USER, {
+			key: "createUser",
+			variables: {
+				data: {
+					profileImage: '',
+					departmentCode: selectedDepartment,
+					name: name,
+					email: email,
+					password: password,
+					roles: selectedRole.filter(role => role != "none"),
+				},
+			},
+		});
+
+		clearInputs()
+		setCreateUserFlag(true);
+		setTimeout(() => setCreateUserFlag(false), 2 * 1000)
+		// location.reload();
+	}
+
 	useEffect(() => {
 		fetchDepartments();
 
 		return () => {};
 	}, []);
 
-	useEffect(() => {
-		console.log(selectedRole);
+	// useEffect(() => {
+	// 	console.log(selectedRole);
 
-		return () => {};
-	}, [selectedRole]);
+	// 	return () => {};
+	// }, [selectedRole]);
 
-	useEffect(() => {
-		console.log(selectedDepartment);
+	// useEffect(() => {
+	// 	console.log(selectedDepartment);
 
-		return () => {};
-	}, [selectedDepartment]);
+	// 	return () => {};
+	// }, [selectedDepartment]);
 
 	return (
 		<>
-			<div className="bg-darkGreen flex flex-col rounded-lg p-3 items-center">
-				<h1 className="font-bold text-[2rem] bg-white w-fit text-darkGreen p-2 rounded-lg mb-5">
+			<div className="bg-gray-900 flex flex-col rounded-lg p-3 py-10 items-center">
+				<h1 className="font-bold text-[2rem] bg-white w-fit text-black p-2 rounded-lg mb-5">
 					Criando um novo usuário
 				</h1>
 
@@ -148,27 +180,16 @@ export default function CreateUser({}: CreateUserProps) {
 							handleChange={handleDepartmentChange}
 						/>
 					</div>
-					<CreateUserButton
-						className="w-fit text-white bg-emerald-800 border-emerald-800"
-						userData={{
-							department: [
-								departmentFromCode(
-									selectedDepartment,
-									departments
-								)!,
-							],
-							// @ts-expect-error
-							roles: selectedRole.filter((r) => r != "none"),
-							profileImage: "",
-							email,
-							password,
-							name,
-						}}
-					>
+					<button className="btn w-fit text-white bg-purple-800 hover:bg-purple-900 border-purple-800 hover:border-purple-900" onClick={handleCreateUser}>
 						Criar
-					</CreateUserButton>
+					</button>
 				</div>
 			</div>
+			{createUserFlag && (
+				<Toast className="alert-success">
+					<span className="text-xl font-bold">Usuário criado com sucesso</span>
+				</Toast>
+			)}
 		</>
 	);
 }
